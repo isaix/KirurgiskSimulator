0c28c48c2b3abad30de8a5589d89bbeb0bfc1d97
fredehus <fredehus@gmail.com>
2018-10-29 11:01:07 +0100

    start på login

--- app/src/main/java/dk/dtu/isaacirani/kirurgisksimulator/MainActivity.java
+++ app/src/main/java/dk/dtu/isaacirani/kirurgisksimulator/MainActivity.java
@@ -3,7 +3,7 @@ package dk.dtu.isaacirani.kirurgisksimulator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class logInActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
