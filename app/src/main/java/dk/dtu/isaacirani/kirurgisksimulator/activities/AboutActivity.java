c9c8865e8cf1281eed506a6a16df8b213bb695e1
daktapus <larsumlau@hotmail.com>
2018-11-26 10:31:34 +0100

    about activity fragment

--- app/src/main/java/dk/dtu/isaacirani/kirurgisksimulator/activities/AboutActivity.java
+++ app/src/main/java/dk/dtu/isaacirani/kirurgisksimulator/activities/AboutActivity.java
@@ -1,10 +1,30 @@
 package dk.dtu.isaacirani.kirurgisksimulator.activities;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import dk.dtu.isaacirani.kirurgisksimulator.R;
 
 public class AboutActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public void hide(View view) {
+
+        @SuppressLint("WrongViewCast") TextView txtView = (TextView)findViewById(R.id.imageView9);
+
+        //Toggle
+        if (txtView.getVisibility() == View.VISIBLE)
+            txtView.setVisibility(View.INVISIBLE);
+        else
+            txtView.setVisibility(View.VISIBLE);
+
+        //If you want it only one time
+        //txtView.setVisibility(View.VISIBLE);
+
+    }
+
     @Override
     public void onClick(View v) {
 
---
+++ app/src/main/java/dk/dtu/isaacirani/kirurgisksimulator/models/AboutUsFragment.java
@@ -0,0 +1,28 @@
+package dk.dtu.isaacirani.kirurgisksimulator.models;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import dk.dtu.isaacirani.kirurgisksimulator.R;
+
+
+
+public class AboutUsFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View about_us = inflater.inflate((R.layout.fragment_about_us), container, false);
+
+
+
+
+        return about_us;
+
+    }
+    }
