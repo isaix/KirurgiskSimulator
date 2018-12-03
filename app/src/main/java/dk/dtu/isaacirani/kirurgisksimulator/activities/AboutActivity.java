fcd7da60907da150f2dc1ca72ace0545abd3eda9
daktapus <larsumlau@hotmail.com>
2018-12-03 10:19:52 +0100

    about fix igen igen....

--- app/src/main/java/dk/dtu/isaacirani/kirurgisksimulator/activities/AboutActivity.java
+++ app/src/main/java/dk/dtu/isaacirani/kirurgisksimulator/activities/AboutActivity.java
@@ -1,8 +1,10 @@
 package dk.dtu.isaacirani.kirurgisksimulator.activities;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 import dk.dtu.isaacirani.kirurgisksimulator.R;
 
@@ -12,6 +14,20 @@ public class AboutActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+    public void hide(View view) {
+
+        @SuppressLint("WrongViewCast") TextView txtView = (TextView)findViewById(R.id.imageView9);
+
+        //Toggle
+        if (txtView.getVisibility() == View.VISIBLE)
+            txtView.setVisibility(View.INVISIBLE);
+        else
+            txtView.setVisibility(View.VISIBLE);
+
+        //If you want it only one time
+        //txtView.setVisibility(View.VISIBLE);
+
     }
 
     @Override
