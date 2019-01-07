1bc9fc0aefbf527fc19798412223b00e0f6ddcf5
Y0SSB0SS <josephine.mellin@gmail.com>
2019-01-07 21:45:41 +0100

    Include layout sådan relativt på plads. Nu skal designet bare laves om.

--- app/src/main/java/dk/dtu/isaacirani/kirurgisksimulator/activities/SimulatorActivity.java
+++ app/src/main/java/dk/dtu/isaacirani/kirurgisksimulator/activities/SimulatorActivity.java
@@ -1,7 +1,9 @@
 package dk.dtu.isaacirani.kirurgisksimulator.activities;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -21,52 +23,58 @@ public class SimulatorActivity extends AppCompatActivity implements View.OnClick
     SimulatorPresenter presenter;
 
     private boolean isOn;
-    private int buttonvalue1 = 00;
-    private int buttonvalue2 = 00;
-    private ImageView barblank1;
-    private ImageView barblank2;
-    private ImageView barblank3;
-    private ImageView barblank4;
-    private ImageButton plusbutton1;
-    private ImageButton minusbutton1;
-    private ImageButton plusbutton2;
-    private ImageButton minusbutton2;
-    private TextView tevalue;
-    private TextView tevalue00;
-    private TextView tevalue000;
+    private int buttonvalue_1 = 00;
+    private int buttonvalue_2 = 00;
+    private ImageView barblank1_1;
+    private ImageView barblank1_2;
+    private ImageView barblank2_1;
+    private ImageView barblank2_2;
+    private TextView totalvalue;
+    private TextView value_1;
+    private TextView value_2;
     private Switch switchbutton;
+    private ImageButton minusbutton_1;
+    private ImageButton plusbutton_1;
+    private ImageButton minusbutton_2;
+    private ImageButton plusbutton_2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_simulator);
+        setContentView(R.layout.activity_simulator_container);
 
         presenter = new SimulatorPresenter(this);
 
+        //laver View @namepåframe = (ImageView) find...(R.id.@navnetpåframe)
+        View frame1 = findViewById(R.id.frame1);
+        View frame2 = findViewById(R.id.frame2);
+        View frame3 = findViewById(R.id.frame3);
+        View frame4 = findViewById(R.id.frame4);
+
+
         //refererer til alle IV, IB og TV
-        barblank1 = (ImageView) findViewById(R.id.barblank1);
-        barblank2 = (ImageView) findViewById(R.id.barblank2);
-        barblank3 = (ImageView) findViewById(R.id.barblank3);
-        barblank4 = (ImageView) findViewById(R.id.barblank4);
-
-        plusbutton1 = (ImageButton) findViewById(R.id.plusbutton1);
-        plusbutton2 = (ImageButton) findViewById(R.id.plusbutton2);
-        minusbutton1 = (ImageButton) findViewById(R.id.minusbutton1);
-        minusbutton2 = (ImageButton) findViewById(R.id.minusbutton2);
-
-        tevalue00 = (TextView) findViewById(R.id.tevalue00);
-        tevalue = (TextView) findViewById(R.id.tevalue);
-        tevalue000 = (TextView) findViewById(R.id.tevalue000);
-        switchbutton = (Switch) findViewById(R.id.switchbutton);
+        switchbutton = (Switch) frame1.findViewById(R.id.switchbutton);
+        barblank1_1 = (ImageView) frame2.findViewById(R.id.barblank1);
+        barblank2_1 = (ImageView) frame2.findViewById(R.id.barblank2);
+        barblank1_2 = (ImageView) frame3.findViewById(R.id.barblank1);
+        barblank2_2 = (ImageView) frame3.findViewById(R.id.barblank2);
+        minusbutton_1 = (ImageButton) frame2.findViewById(R.id.minusbutton);
+        plusbutton_1 = (ImageButton) frame2.findViewById(R.id.plusbutton);
+        minusbutton_2 = (ImageButton) frame3.findViewById(R.id.minusbutton);
+        plusbutton_2 = (ImageButton) frame3.findViewById(R.id.plusbutton);
+        value_1 = (TextView) frame2.findViewById(R.id.value);
+        value_2 = (TextView) frame3.findViewById(R.id.value);
+        totalvalue = (TextView) frame4.findViewById(R.id.totalvalue);
 
         //setter onclick på knapperne
-        plusbutton1.setOnClickListener(this);
-        plusbutton2.setOnClickListener(this);
-        minusbutton1.setOnClickListener(this);
-        minusbutton2.setOnClickListener(this);
-        tevalue000.setOnClickListener(this);
-        tevalue00.setOnClickListener(this);
-        tevalue.setOnClickListener(this);
+        plusbutton_1.setOnClickListener(this);
+        minusbutton_1.setOnClickListener(this);
+        plusbutton_2.setOnClickListener(this);
+        minusbutton_2.setOnClickListener(this);
+        value_1.setOnClickListener(this);
+        value_2.setOnClickListener(this);
+        totalvalue.setOnClickListener(this);
         switchbutton.setOnCheckedChangeListener(this);
     }
 
@@ -74,26 +82,27 @@ public class SimulatorActivity extends AppCompatActivity implements View.OnClick
     @Override
     public void onClick(View view) {
         if (isOn) {
-            if (plusbutton1 == view && buttonvalue1 < 12) {
-                buttonvalue1++;
-                tevalue00.setText(String.valueOf(buttonvalue1));
+            if (plusbutton_1 == view && buttonvalue_1 < 12) {
+                buttonvalue_1++;
+                value_1.setText(String.valueOf(buttonvalue_1));
                 update1stBar();
                 update2ndBar();
-            } else if (minusbutton1 == view && buttonvalue1 > 0) {
-                buttonvalue1--;
-                tevalue00.setText(String.valueOf(buttonvalue1));
+            } else if (minusbutton_1 == view && buttonvalue_1 > 0) {
+                buttonvalue_1--;
+                value_1.setText(String.valueOf(buttonvalue_1));
                 update1stBar();
                 update2ndBar();
             }
 
-            if (plusbutton2 == view && buttonvalue2 < 12) {
-                buttonvalue2++;
-                tevalue.setText(String.valueOf(buttonvalue2));
+            if (plusbutton_2 == view && buttonvalue_2 < 12) {
+                Log.e(buttonvalue_1+"", "yeees");
+                buttonvalue_2++;
+                value_2.setText(String.valueOf(buttonvalue_2));
                 update3rdBar();
                 update4thBar();
-            } else if (minusbutton2 == view && buttonvalue2 > 0) {
-                buttonvalue2--;
-                tevalue.setText(String.valueOf(buttonvalue2));
+            } else if (minusbutton_2 == view && buttonvalue_2 > 0) {
+                buttonvalue_2--;
+                value_2.setText(String.valueOf(buttonvalue_2));
                 update3rdBar();
                 update4thBar();
             }
@@ -102,48 +111,48 @@ public class SimulatorActivity extends AppCompatActivity implements View.OnClick
 
     @Override
         public void update1stBar() {
-            switch (buttonvalue1 / 2) {
+            switch (buttonvalue_1 / 2) {
                 case 1:
-                    barblank1.setImageResource(R.drawable.bar1);
+                    barblank1_1.setImageResource(R.drawable.bar1);
                     break;
                 case 2:
-                    barblank1.setImageResource(R.drawable.bar2);
+                    barblank1_1.setImageResource(R.drawable.bar2);
                     break;
                 case 3:
-                    barblank1.setImageResource(R.drawable.bar3);
+                    barblank1_1.setImageResource(R.drawable.bar3);
                     break;
                 case 4:
-                    barblank1.setImageResource(R.drawable.bar4);
+                    barblank1_1.setImageResource(R.drawable.bar4);
                     break;
                 case 5:
-                    barblank1.setImageResource(R.drawable.bar5);
+                    barblank1_1.setImageResource(R.drawable.bar5);
                     break;
                 case 6:
-                    barblank1.setImageResource(R.drawable.bar6);
+                    barblank1_1.setImageResource(R.drawable.bar6);
                 default:
                     break;
             }
         }
     @Override
         public void update2ndBar() {
-            switch (buttonvalue1 / 2) {
+            switch (buttonvalue_1 / 2) {
                 case 1:
-                    barblank2.setImageResource(R.drawable.bar1);
+                    barblank2_1.setImageResource(R.drawable.bar1);
                     break;
                 case 2:
-                    barblank2.setImageResource(R.drawable.bar2);
+                    barblank2_1.setImageResource(R.drawable.bar2);
                     break;
                 case 3:
-                    barblank2.setImageResource(R.drawable.bar3);
+                    barblank2_1.setImageResource(R.drawable.bar3);
                     break;
                 case 4:
-                    barblank2.setImageResource(R.drawable.bar4);
+                    barblank2_1.setImageResource(R.drawable.bar4);
                     break;
                 case 5:
-                    barblank2.setImageResource(R.drawable.bar5);
+                    barblank2_1.setImageResource(R.drawable.bar5);
                     break;
                 case 6:
-                    barblank2.setImageResource(R.drawable.bar6);
+                    barblank2_1.setImageResource(R.drawable.bar6);
                 default:
                     break;
             }
@@ -151,48 +160,48 @@ public class SimulatorActivity extends AppCompatActivity implements View.OnClick
 
     @Override
         public void update3rdBar() {
-            switch (buttonvalue2 / 2) {
+            switch (buttonvalue_2 / 2) {
                 case 1:
-                    barblank3.setImageResource(R.drawable.bar1);
+                    barblank1_2.setImageResource(R.drawable.bar1);
                     break;
                 case 2:
-                    barblank3.setImageResource(R.drawable.bar2);
+                    barblank1_2.setImageResource(R.drawable.bar2);
                     break;
                 case 3:
-                    barblank3.setImageResource(R.drawable.bar3);
+                    barblank1_2.setImageResource(R.drawable.bar3);
                     break;
                 case 4:
-                    barblank3.setImageResource(R.drawable.bar4);
+                    barblank1_2.setImageResource(R.drawable.bar4);
                     break;
                 case 5:
-                    barblank3.setImageResource(R.drawable.bar5);
+                    barblank1_2.setImageResource(R.drawable.bar5);
                     break;
                 case 6:
-                    barblank3.setImageResource(R.drawable.bar6);
+                    barblank1_2.setImageResource(R.drawable.bar6);
                 default:
                     break;
             }
         }
     @Override
         public void update4thBar () {
-            switch (buttonvalue2 / 2) {
+            switch (buttonvalue_2 / 2) {
                 case 1:
-                    barblank4.setImageResource(R.drawable.bar1);
+                    barblank2_2.setImageResource(R.drawable.bar1);
                     break;
                 case 2:
-                    barblank4.setImageResource(R.drawable.bar2);
+                    barblank2_2.setImageResource(R.drawable.bar2);
                     break;
                 case 3:
-                    barblank4.setImageResource(R.drawable.bar3);
+                    barblank2_2.setImageResource(R.drawable.bar3);
                     break;
                 case 4:
-                    barblank4.setImageResource(R.drawable.bar4);
+                    barblank2_2.setImageResource(R.drawable.bar4);
                     break;
                 case 5:
-                    barblank4.setImageResource(R.drawable.bar5);
+                    barblank2_2.setImageResource(R.drawable.bar5);
                     break;
                 case 6:
-                    barblank4.setImageResource(R.drawable.bar6);
+                    barblank2_2.setImageResource(R.drawable.bar6);
                 default:
                     break;
             }
@@ -219,9 +228,9 @@ public class SimulatorActivity extends AppCompatActivity implements View.OnClick
 
     @Override
     public void changeDisplayValues(Scenario scenario) {
-        tevalue00.setText(String.valueOf(scenario.getRate()));
-        tevalue000.setText(String.valueOf(scenario.getVolume()));
-        tevalue.setText(String.valueOf(scenario.getPressure()));
+        value_1.setText(String.valueOf(scenario.getRate()));
+        totalvalue.setText(String.valueOf(scenario.getVolume()));
+        value_2.setText(String.valueOf(scenario.getPressure()));
         update1stBar();
         update2ndBar();
         update3rdBar();
